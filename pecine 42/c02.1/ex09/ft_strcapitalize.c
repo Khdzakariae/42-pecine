78d090af12c98d6fa05285f0b16d0f2ec1654eb2
zakaria <elkhadirzakaria48@gmail.com>
2023-08-30 18:51:55 +0100

    pecine

---
+++ pecine 42/c02.1/ex09/ft_strcapitalize.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-khad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 09:01:32 by zel-khad          #+#    #+#             */
+/*   Updated: 2023/07/22 13:35:41 by zel-khad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 1;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (j == 1)
+			{
+				str[i] -= 32;
+				j = 0;
+			}
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			j = 0;
+		else
+			j = 1;
+		i++;
+	}
+	return (str);
+}
---
+++ pecine 42/c02.1/ex10/ft_strlcpy.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zel-khad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 16:09:39 by zel-khad          #+#    #+#             */
+/*   Updated: 2023/07/22 11:39:43 by zel-khad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *src)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	len;
+
+	i = 0;
+	while (i < size -1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	len = ft_strlen(src);
+	return (len);
+}
